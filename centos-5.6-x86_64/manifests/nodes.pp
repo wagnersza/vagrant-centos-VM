e356a8c45311918e0a96a1a3f45848f3dcf0d41c
Wagner Souza <wagnersza@corp.globo.com>
2011-07-14 17:00:12 -0300

    vagrant working

--- centos-5.6-x86_64/manifests/nodes.pp
+++ centos-5.6-x86_64/manifests/nodes.pp
@@ -1,21 +1,32 @@
 node basenode {
+	$puppet_master = "puppetmaster"
+	$zone = "vagrant"
+	$user_password = '$1$NrB1y8wm$APXJ5z4uE2.1uZ5C6NIB81'
 	include common
+	#include puppet::common
 }
 
 node sandbox inherits basenode {
 	$zone = "sandbox"
 	$puppet_master = "puppet.local.com"
-	$user_password = '$1$NrB1y8wm$APXJ5z4uE2.1uZ5C6NIB81'
 	include user::puppet
 	exec { "route add -net 10.2.0.0 netmask 255.255.0.0 gw 33.33.33.1 eth1":
 	        unless => 'netstat -nrv | grep -e "10.2.0.0.*33.33.33.1"',
 	}
 }
 
-# node puppetmaster inherits sandbox {
-#         
-# 		$puppetmaster_passwd = '$1$6FlrakZW$7P7npg/7rbgAt2r8hrJvC0'
-# 		$puppetmaster_local_ipbind = '33.33.33.33'
-# 		
-# 		include role_puppetmaster
-# }
+node puppetmaster inherits sandbox {
+        
+		$puppetmaster_passwd = '$1$6FlrakZW$7P7npg/7rbgAt2r8hrJvC0'
+		$puppetmaster_local_ipbind = '33.33.33.33'
+		
+		include role_puppetmaster
+}
+
+node puppetclient inherits sandbox {
+        
+		$puppetclient_passwd = '$1$6FlrakZW$7P7npg/7rbgAt2r8hrJvC0'
+		$puppetclient_local_ipbind = '33.33.33.34'
+		
+		include role_puppetclient
+}
