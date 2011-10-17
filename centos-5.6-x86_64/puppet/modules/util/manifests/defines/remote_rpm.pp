89054c99a590df5ff46697c85acbfc45ffa5a456
Wagner Souza <wagnersza@corp.globo.com>
2011-10-17 14:20:14 -0200

    add rpm ruby-1.9.3_rc1 and puppet-dashboard-1.2.2

--- centos-5.6-x86_64/puppet/modules/util/manifests/defines/remote_rpm.pp
+++ centos-5.6-x86_64/puppet/modules/util/manifests/defines/remote_rpm.pp
@@ -4,16 +4,22 @@
 #
 # Author: Wagner Souza (wagnersza@gmail.com)
 #
-define remote_rpm ($host,$package,$path = "/tmp"){
+define remote_rpm (${host},${package},${path} = "/tmp"){
 
-  exec { "wget_file":
-    command => "wget -q -O $path/$package $host/$package",
-    onlyif => "test ! -f $path/$package",
+  # exec { "wget_file":
+  #   command => "wget -q -O $path/$package $host/$package",
+  #   onlyif => "test ! -f $path/$package",
+  # }
+  
+  exec { "get_file":
+    command => "curl -s ${host}/${package} > ${path}/${package}",
+    onlyif => "test ! -f ${path}/${package}",
   }
+  
 
-  #   package {"$package":
+  #   package {"${package}":
   #   provider => yum,
-  #   source => "$path",
+  #   source => "${path}",
   #   require => [ Exec["wget_file"] ],   
   # }
 }
